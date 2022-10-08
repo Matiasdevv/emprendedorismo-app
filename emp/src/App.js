6eeda0a30726d9600c4c78474099fdd9980bfe51
Matiasdevv <gaunam134@gmail.com>
2022-10-07 23:21:58 -0300

    agrege login

--- emp/src/App.js
+++ emp/src/App.js
@@ -1,22 +1,33 @@
-import { Fragment, useEffect} from 'react'
+import { Fragment, useEffect, useState} from 'react'
 import { Homepage } from './components/Homepage';
 import { SideBar } from './pages/SideBar';
 import {BrowserRouter as Router, Route, Link, Routes, useNavigate} from 'react-router-dom'
+import { LoginForm } from './form/LoginForm';
 function App() {
 
-  // COsas a arreglar / implementar: 
+  // COsas a arreglar / implementar:
   //Apartados, jerarquia, planillas(modelo), crear tabla de division de cursos, sidebar dinamiko, apartado de orientacion en sidebar
+
+  const [logged, setLogged] = useState(false)
+
+  useEffect(() => {
+    setLogged(localStorage.getItem('logged'))
+
+  }, [])
 
 
   return (
     <Fragment>
 
-      <div className='d-flex'>
+            <div className='d-flex'>
 
-        <SideBar/> 
-          
+        { logged ? <SideBar/> : <LoginForm/>}
+
 
       </div>
+
+
+
     </Fragment>
   );
 }
